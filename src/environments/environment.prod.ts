30263db25cf7a8cb3f96bba7ce7015a1c9f8fd9c
rogermoka <roger.mokarzel@gmail.com>
2019-01-10 15:51:17 -0200

    278. Creating a New Project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDOBdVS7cPVZtLeIfx65SETIXe-27RmSWY',
+    authDomain: 'oshop-66e57.firebaseapp.com',
+    databaseURL: 'https://oshop-66e57.firebaseio.com',
+    projectId: 'oshop-66e57',
+    storageBucket: '',
+    messagingSenderId: '636468105333'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDOBdVS7cPVZtLeIfx65SETIXe-27RmSWY',
+    authDomain: 'oshop-66e57.firebaseapp.com',
+    databaseURL: 'https://oshop-66e57.firebaseio.com',
+    projectId: 'oshop-66e57',
+    storageBucket: '',
+    messagingSenderId: '636468105333'
+  }
 };
